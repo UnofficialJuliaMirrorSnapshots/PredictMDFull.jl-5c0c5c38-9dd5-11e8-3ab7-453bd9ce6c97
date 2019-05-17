92674ae907746662a92af98696c24146d95ae5f5
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-05-17 05:32:35 +0000

    Snapshot of branch master taken on 2019-05-17T01:32:33.914-04:00 from "https://github.com/UnofficialJuliaMirror/PredictMDFull.jl-5c0c5c38-9dd5-11e8-3ab7-453bd9ce6c97"

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -6,7 +6,7 @@ set -ev
 
 julia --check-bounds=yes --color=yes -e '
     import Pkg;
-    Pkg.Registry.add(Pkg.RegistrySpec(name="PredictMDRegistry",url="https://github.com/bcbi/PredictMDRegistry.git",uuid="26a550a3-39fe-4af4-af6d-e8814c2b6dd9",));
+    Pkg.Registry.add(Pkg.RegistrySpec(name="BCBIRegistry",url="https://github.com/bcbi/BCBIRegistry.git",uuid="26a550a3-39fe-4af4-af6d-e8814c2b6dd9",));
     '
 
 julia --check-bounds=yes --color=yes -e '
